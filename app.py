763381a38fa03dda94206216e9882927fdd68906
Sabine <sabine@21.lv>
2020-05-26 22:37:23 +0200

    add date picker

--- app.py
+++ app.py
@@ -17,8 +17,12 @@ mongo = PyMongo(app)
 @app.route('/')
 @app.route('/get_tasks')
 def get_tasks():
-    task12 = mongo.db.tasks.find()
-    return render_template("tasks.html", tasks=task12)
+    return render_template("tasks.html", tasks=mongo.db.tasks.find())
+
+
+@app.route('/add_task')
+def add_task():
+    return render_template('addtask.html')
 
 
 if __name__ == '__main__':
